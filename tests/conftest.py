5a4d5d8342d0ed6bd3906406f8236d6089488563
Howard <howard.schneider@gmail.com>
2025-10-19 21:15:07 -0400

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# tests/conftest.py
-import os, sys
-
-# Put the repo root (directory that contains cca8_world_graph.py) on sys.path
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if ROOT not in sys.path:
-    sys.path.insert(0, ROOT)
