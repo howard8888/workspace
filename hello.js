8f3367175f34a2415956588738002dca76a1ee34
howard <howard.schneider@gmail.com>
2026-02-04 12:35:44 -0500

    test envrt

---
+++ hello.js
@@ -0,0 +1,5 @@
+function greet(name) {
+    return "Hello, " + name + " :)";
+}
+var who = "Howard";
+console.log(greet(who));
---
+++ hello.ts
@@ -0,0 +1,8 @@
+
+
+function greet(name: string): string{
+	return "Hello, " + name + " :)";
+}
+
+const who: string = "Howard";
+console.log(greet(who));
